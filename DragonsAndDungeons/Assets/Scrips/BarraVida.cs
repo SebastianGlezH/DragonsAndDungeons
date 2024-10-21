0ee0b555777a1263b0b919eea472fcda38b62817
Miguellc0 <miguel1asturias@gmail.com>
2024-10-20 18:01:00 -0600

    Barra de vida

---
+++ DragonsAndDungeons/Assets/Scrips/BarraVida.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Image = UnityEngine.UI.Image;
+using static System.Net.Mime.MediaTypeNames;
+
+public class BarraVida : MonoBehaviour
+{
+
+    public Image Lavida;
+    public float saludMaxima;
+    public float porcentajeVida;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        saludMaxima = 100f;
+        porcentajeVida = 100f;
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("RestarVida"))
+        {
+            porcentajeVida -= 20f;
+        }
+
+    }
+
+
+    void Update()
+    {
+        Lavida.fillAmount = porcentajeVida / saludMaxima;
+    }
+
+
+}
